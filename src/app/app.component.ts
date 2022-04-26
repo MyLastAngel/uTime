11eb89dbc70acd6bd9e1f0be4f6b000fe17cb6c6
s.naiden <s.naiden@rlt.ru>
2022-04-26 18:22:13 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'u-time-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'uTime';
+}
