b935d400d997ce2c293012126d1888dc08fe6815
s.naiden <s.naiden@rlt.ru>
2022-04-28 16:48:32 +0300

    - Структура проекта - Локальный линк к модулям

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+import { PlannerModule } from '@ns/planner';
+import { AppComponent } from './visual/app.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PlannerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
