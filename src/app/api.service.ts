b16d68fec6a2904ba3a597896b241fe53e19c7a7
s.naiden <s.naiden@rlt.ru>
2022-05-20 16:33:25 +0300

    Пример подмены API

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,11 @@
-/* import { ApiService } from "@ns/planner";
+import { Injectable } from "@angular/core";
+import { ApiService } from "@ns/planner";
 
+@Injectable({ providedIn : 'root' })
 export class HttpService implements ApiService
 {
-
-} */
+    public load() : void
+    {
+        console.warn("uTime");
+    }
+} 
